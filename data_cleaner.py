d6401d99696c922a96a35bc84e29f67d65130664
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2023-11-09 19:00:41 -0500

    Add pointer curation

--- data_cleaner.py
+++ data_cleaner.py
@@ -6,17 +6,20 @@ This module:
 """
 
 import os
+import json
 import pandas as pd
 
 class DataCleaner:
     """
     Calling class for the library.
     """
+
     def __init__(self) -> None:
         """
         Catch all datasets in the watch folder
         """
         self.datasets = os.listdir('./datasets/')
+        self.handles = []
 
     def data_catch_status(self):
         """
@@ -31,7 +34,7 @@ class DataCleaner:
 
         return len(self.datasets)
 
-    def get_dataset_pointer(self):
+    def _create_dataset_pointers(self, title, ext):
         """
         Not all datasets can be csvs, so this will read all types of datasets.
         This routine will also read data from data_config.json to get even better help!
@@ -41,5 +44,49 @@ class DataCleaner:
             A file shades_of_color.json must be present to enable this!
         """
 
-        self.handles = []
+        if ext in 'json':
+            return
 
+        params = None
+        if f'{title}.json' in self.datasets:
+            params = json.load(open(f'{title}.json', 'r'))
+
+        df = None
+        loc = f'./{title}.{ext}'
+
+        if ext == 'csv':
+            if params:
+                df = pd.read_csv(loc, **params)
+            else:
+                df = pd.read_csv(loc)
+        elif ext in ['xls', 'xlsx']:
+            if params:
+                df = pd.read_excel(loc, **params)
+            else:
+                df = pd.read_excel(loc)
+        elif ext == 'json':
+            if params:
+                df = pd.read_json(loc, **params)
+            else:
+                df = pd.read_json(loc)
+        elif ext in ['html', 'htm']:
+            if params:
+                df = pd.read_html(loc, **params)[0]
+            else:
+                df = pd.read_html(loc)[0]
+
+        self.handles.append(df)
+
+    def curate_dataset_pointers(self):
+        """
+        Execute this subroutine to read all datasets.
+        You will be warned if any of your config is wrong, or if file is not supported.
+        """
+
+        for file_name in self.datasets:
+            title, ext = file_name.split('.')
+
+            try:
+                self._create_dataset_pointers(title, ext)
+            except Exception as exc:
+                raise(f'LOG:: {title}.{ext} failed reading.' + exc)
