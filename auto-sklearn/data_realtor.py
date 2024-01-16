4064069579972a15a6f419e2e1d5f1cd1eeb9d9b
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2024-01-16 04:31:07 -0500

    HARD RESTART

---
+++ auto-sklearn/data_realtor.py
@@ -0,0 +1,14 @@
+"""
+DataRealtor converts a dataset into modelling food.
+"""
+
+import os
+import pandas as pd
+
+class DataRealtor:
+    """
+    This module:
+    - Fetches datasets you provide
+    - Cleans any unsuitable values
+    - Morphs datasets into modelling friendly structure
+    """
