2bf45e2109495279a2a67b57dbef56a08675ec3d
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2024-01-16 10:10:38 -0500

    Add Outlier Manager

--- auto-sklearn/data_realtor.py
+++ auto-sklearn/data_realtor.py
@@ -30,3 +30,29 @@ class DataRealtor:
 
         return df.dropna(axis=0)
     
+    def _manage_outlier(self):
+        """
+        Tukey's Method to remove outliers.
+        This will improve model accuracy. Since purpose of this repository is preliminary evaluation.
+        We can afford data loss.
+        """
+
+        q1 = self.y.quantile(0.25)
+        q3 = self.y.quantile(0.75)
+
+        # Inter Quartile Range
+        iqr = q3 - q1
+
+        # Finding the lower and upper bounds for outlier detection
+        lower_bound = q1 - 1.5 * iqr
+        upper_bound = q3 + 1.5 * iqr
+
+        # Find outlier rows on both sides
+        is_outlier_lower = (self.y < lower_bound)
+        is_outlier_upper = (self.y > upper_bound)
+
+        is_outlier = is_outlier_lower | is_outlier_upper
+
+        self.x = self.x[~is_outlier]
+        self.y = self.y[~is_outlier]
+
