18e13ff8c35adbaa2f608081d08c430d1163a21d
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2024-01-16 05:46:23 -0500

    HARD RESTART

--- auto-sklearn/data_realtor.py
+++ auto-sklearn/data_realtor.py
@@ -12,3 +12,20 @@ class DataRealtor:
     - Cleans any unsuitable values
     - Morphs datasets into modelling friendly structure
     """
+
+    def __init__(self, path_to_file, target_feature) -> None:
+        self.target = target_feature
+
+        df = pd.read_csv(path_to_file)
+        df = self._manage_missing(df)
+
+        # Separating Target and Features
+        self.x, self.y = df.pop(target_feature)
+
+    def _manage_missing(self, df):
+        """
+        Removes any row with missing value.
+        !! Desired Imputation may be implemented here !!
+        """
+
+        return df.dropna(axis=0)
