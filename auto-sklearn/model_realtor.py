9f13f35716a924227142d7617033603e1c785853
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2024-01-18 04:02:49 -0500

    Add ModelRealtor docstring

---
+++ auto-sklearn/model_realtor.py
@@ -0,0 +1,49 @@
+"""
+ModelRealtor trains the following models.
+
+Regression Models:
+1. Linear Regression
+2. Ridge Regression
+3. Lasso Regression
+4. ElasticNet Regression
+5. Polynomial Regression
+6. Support Vector Regression (SVR)
+7. Decision Tree Regression
+8. Random Forest Regression
+9. Gradient Boosting Regression
+10. AdaBoost Regression
+11. Bayesian Ridge Regression
+12. Passive Aggressive Regression
+13. Huber Regression
+14. Theil-Sen Regression
+15. LARS Regression
+16. RANSAC Regression
+17. Gaussian Process Regression
+18. Isotonic Regression
+
+Classification Models:
+1. Logistic Regression
+2. K-Nearest Neighbors (KNeighborsClassifier)
+3. Support Vector Machines (SVM) for Classification (SVC)
+4. Stochastic Gradient Descent (SGD) Classifier
+5. Decision Tree Classifier
+6. Random Forest Classifier
+7. Gradient Boosting Classifier (GradientBoostingClassifier)
+8. AdaBoost Classifier (AdaBoostClassifier)
+9. Multilayer Perceptron Classifier (MLPClassifier)
+10. Gaussian Naive Bayes (GaussianNB)
+11. Bernoulli Naive Bayes (BernoulliNB)
+12. Complement Naive Bayes (ComplementNB)
+13. Multinomial Naive Bayes (MultinomialNB)
+14. Passive Aggressive Classifier
+15. Quadratic Discriminant Analysis (QDA)
+16. Linear Discriminant Analysis (LDA)
+17. Nu-Support Vector Classification
+18. One-Class SVM (OneClassSVM)
+"""
+
+import pandas as pd
+
+class ModelRealtor:
+    def __init__(self) -> None:
+        pass
